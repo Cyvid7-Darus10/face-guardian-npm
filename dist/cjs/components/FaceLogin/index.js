67bef6cd9453aa74c249a5542fe82c577e867538
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-05-29 00:49:21 +0800

    fix: added checker for undefined window

--- dist/cjs/components/FaceLogin/index.js
+++ dist/cjs/components/FaceLogin/index.js
@@ -4,32 +4,36 @@ var tslib_1 = require("tslib");
 var react_1 = tslib_1.__importStar(require("react"));
 var FaceLogin = function (_a) {
     var appId = _a.appId, buttonStyles = _a.buttonStyles, _b = _a.buttonText, buttonText = _b === void 0 ? 'Face Guardian' : _b;
-    var currentUrl = window.location.href;
+    var currentUrl = typeof window !== 'undefined' ? window.location.href : '';
     var handleButtonClick = function () {
-        window.location.href = "https://www.face-guardian.com/login?appId=".concat(appId, "&redirectUrl=").concat(currentUrl);
+        if (typeof window !== 'undefined') {
+            window.location.href = "https://www.face-guardian.com/login?appId=".concat(appId, "&redirectUrl=").concat(currentUrl);
+        }
     };
     (0, react_1.useEffect)(function () {
-        // Check for authorization code in URL
-        var urlParams = new URLSearchParams(window.location.search);
-        var authorizationCode = urlParams.get('authorizationCode');
-        var redirectUrl = urlParams.get('redirectUrl');
-        if (authorizationCode) {
-            // Call API with authorization code
-            fetch('https://www.face-guardian.com/api/request-token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ authorizationCode: authorizationCode })
-            })
-                .then(function (response) { return response.json(); })
-                .then(function (data) {
-                // Save token in local storage
-                localStorage.setItem('token', data.token);
-                if (redirectUrl) {
-                    window.location.href = redirectUrl;
-                }
-            })["catch"](function (err) { return console.error(err); });
+        if (typeof window !== 'undefined') {
+            // Check for authorization code in URL
+            var urlParams = new URLSearchParams(window.location.search);
+            var authorizationCode = urlParams.get('authorizationCode');
+            var redirectUrl_1 = urlParams.get('redirectUrl');
+            if (authorizationCode) {
+                // Call API with authorization code
+                fetch('https://www.face-guardian.com/api/request-token', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ authorizationCode: authorizationCode })
+                })
+                    .then(function (response) { return response.json(); })
+                    .then(function (data) {
+                    // Save token in local storage
+                    localStorage.setItem('token', data.token);
+                    if (redirectUrl_1) {
+                        window.location.href = redirectUrl_1;
+                    }
+                })["catch"](function (err) { return console.error(err); });
+            }
         }
     }, []);
     var defaultStyles = {
